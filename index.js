bdaf24abfee1524b8199e13925296b03bdf8c74a
unknown <federico.stagnaro.ar@gmail.com>
2022-01-21 14:24:33 -0300

    add react build

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const cors = require("cors")
 
-
 const app = express()
 
 app.use(cors())
+
+app.use(express.static("build"))
 
 app.use(express.json()) // parser para ingresos de datos json
 
