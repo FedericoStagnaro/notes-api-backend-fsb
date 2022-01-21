02db0dfb1445191f98ac2ba6a58d6682da244fb3
unknown <federico.stagnaro.ar@gmail.com>
2022-01-20 22:14:55 -0300

    my first api

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "node":true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": "latest"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
+}
---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json()) // parser para ingresos de datos json
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  response.json(note)
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(nota => nota.id !== id)
+  console.log(notes)
+  res.status(204).end()
+})
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes)
+})
+
+const generateID = () => {
+  const maxID = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  return maxID + 1
+}
+
+app.post("/api/notes/", (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({ error: "Content Missing" })
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateID()
+  }
+  notes = notes.concat(note)
+
+  res.json(note)
+})
+
+app.use((request,response)=>{
+  console.log("hola")
+  response.status(404).json({
+    error: "Error 404, pagina no encontrada..."})
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
