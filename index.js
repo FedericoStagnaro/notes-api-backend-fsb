b536795d453420f2415bd3d89efee20fcbb28a6b
unknown <federico.stagnaro.ar@gmail.com>
2022-01-20 22:57:41 -0300

    add heroku config

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use((request,response)=>{
     error: "Error 404, pagina no encontrada..."})
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
